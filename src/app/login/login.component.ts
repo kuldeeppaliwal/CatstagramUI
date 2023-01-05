d746a793d76b50956a3a317f27b0a023feacf4b8
kuldeeppaliwal <kuldeeppaliwal@hotmail.com>
2023-01-06 01:52:52 +0530

    Module 2 : Reactive Login Form Created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      'username': [''],
+      'password': ['']
+    });
+  }
 
   ngOnInit(): void {
   }
-
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
